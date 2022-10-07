e6c9535d7605b23c57d414fa5fe8455598c6c809
Roman Nehaikov <rnehaikov@vertafore.com>
2022-10-07 12:26:00 +0300

    Debugging

---
+++ (2)Debugging/Task1/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Product)
+            {
+                Product prod = obj as Product;
+                return this.Price == prod.Price && this.Name.Equals(prod.Name);
+            }
+
+            return false;
+        }
+    }
+}
