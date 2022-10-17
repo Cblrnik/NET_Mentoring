371f67abac4e1ebe75e31b50225299fd85fe3c6c
Roman Nehaikov <rnehaikov@vertafore.com>
2022-10-17 17:48:35 +0300

    Advanced

---
+++ Advanced/FileSystemVisitor.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Advanced
+{
+    public class FileSystemVisitor
+    {
+        private readonly Predicate<string> filter;
+        private readonly string startFolder;
+        private DirectoryInfo directoryInfo;
+        private bool isFirstDirectory;
+        private readonly bool isFiltered;
+        public event Notifier Notificator;
+        public event FileFound FileFound;
+        public event DirectoryFound DirectoryFound;
+        public event FilteredFileFound FilteredFileFound;
+        public event FilteredDirectoryFound FilteredDirectoryFound;
+        public List<string> excludedNames = new List<string>();
+
+        public FileSystemVisitor(string startFolder)
+        {
+            this.startFolder = startFolder ?? throw new ArgumentNullException(nameof(startFolder));
+            this.CreateDirectory();
+            this.isFirstDirectory = true;
+            this.isFiltered = false;
+        }
+
+        public FileSystemVisitor(string startFolder, Predicate<string> filter)
+        {
+            this.startFolder = startFolder ?? throw new ArgumentNullException(nameof(startFolder));
+            this.filter = filter ?? throw new ArgumentNullException(nameof(filter));
+            this.CreateDirectory();
+            this.isFirstDirectory = true;
+            this.isFiltered = true;
+        }
+
+        public void StartSearch()
+        {
+            this.Notificator?.Invoke("Start");
+            this.Search(this.directoryInfo, 0);
+            if (isFiltered)
+            {
+                Console.WriteLine("----------------------------------------------Filtered----------------------------------------------");
+                this.FilteredSearch(this.directoryInfo, 0);
+            }
+
+            this.Notificator?.Invoke("Finish");
+        }
+
+        private void FilteredSearch(DirectoryInfo info, int tabsCount)
+        {
+            this.FilteredDirectoryFound?.Invoke(info.Name);
+            string tabs = string.Empty;
+            for (int i = 0; i < tabsCount; i++)
+            {
+                tabs += "  ";
+            }
+            if (this.isNeedToPrint(info.Name))
+            {
+                Console.WriteLine(tabs + info.Name + "/");
+                var directories = info.GetDirectories();
+                directoryOutput(info);
+                this.isFirstDirectory = false;
+                foreach (var directory in directories)
+                {
+                    this.FilteredSearch(directory, tabsCount + 1);
+                }
+            }
+
+            void directoryOutput(DirectoryInfo directory)
+            {
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    if (this.isNeedToPrint(file.Name))
+                    {
+                        this.FilteredFileFound?.Invoke(file.Name);
+                        Console.WriteLine("  " + tabs + file.Name);
+                    }
+                }
+            }
+        }
+
+        private void Search(DirectoryInfo info, int tabsCount)
+        {
+            this.DirectoryFound?.Invoke(info.Name);
+            string tabs = string.Empty;
+            for (int i = 0; i < tabsCount; i++)
+            {
+                tabs += "  ";
+            }
+
+            Console.WriteLine(tabs + info.Name + "/");
+            var directories = info.GetDirectories();
+            directoryOutput(info);
+            foreach (var directory in directories)
+            {
+                this.Search(directory, tabsCount + 1);
+            }
+
+            void directoryOutput(DirectoryInfo directory)
+            {
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    this.FileFound?.Invoke(file.Name);
+                    Console.WriteLine("  " + tabs + file.Name);
+                }
+            }
+        }
+
+        public void StopSearch()
+        {
+            this.Notificator?.Invoke("Finish");
+            Environment.Exit(0);
+        }
+
+        private bool CheckByName(string name)
+        {
+            return this.filter?.Invoke(name) ?? true;
+        }
+
+        private bool isNeedToPrint(string name)
+        {
+            return !this.excludedNames.Contains(name) && (this.CheckByName(name) || this.isFirstDirectory);
+        }
+
+        private void CreateDirectory()
+        {
+            if (Directory.Exists(this.startFolder))
+            {
+                this.directoryInfo = new DirectoryInfo(startFolder);
+            }
+            else
+            {
+                throw new ArgumentException("There is no such directory");
+            }
+        }
+    }
+}
---
+++ Advanced/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Advanced
+{
+    public delegate void Notifier(string step);
+    public delegate void FileFound(string name);
+    public delegate void DirectoryFound(string name);
+    public delegate void FilteredFileFound(string name);
+    public delegate void FilteredDirectoryFound(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemVisitor file = new FileSystemVisitor("D:\\Temp", (name) => name.Contains("open"));
+            #region Examples
+            file.Notificator += (step) => Console.WriteLine(step);
+            file.FilteredFileFound += (name) =>
+            {
+                if (name.StartsWith("s"))
+                {
+                    Console.WriteLine("Some Action");
+                }
+            };
+            file.FileFound += (name) =>
+            {
+                if (name.Contains(".html"))
+                {
+                    file.excludedNames.Add(name);
+                }
+            };
+
+            file.FilteredDirectoryFound += (name) =>
+            {
+                if (name.Contains("services"))
+                {
+                    file.StopSearch();
+                }
+            };
+            #endregion
+            file.StartSearch();
+        }
+    }
+}
