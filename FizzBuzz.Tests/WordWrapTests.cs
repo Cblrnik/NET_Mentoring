4d4de08274b95e5555b74ae33ce56dbf1a682f9f
Roman Nehaikov <Roman_Nehaikov@epam.com>
2022-11-29 16:52:31 +0300

    Added second kata

---
+++ FizzBuzz.Tests/WordWrapTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class WordWrapTests
+    {
+        [Test]
+        public void GetCountOfNonSpaceSplitters_ValueIsNull_ReturnsZero()
+        {
+            string[] nullValue = null;
+            var countOfSplitters = WordWrap.GetCountOfNonSpaceSplitters(ref nullValue);
+
+            Assert.That(countOfSplitters, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetCountOfNonSpaceSplitters_AllValuesAreValid_ReturnsCountOfValidSplitters()
+        {
+            var baseValue = new string[] { "test", "equals", "null" };
+            var splitters = new string[3];
+            Array.Copy(baseValue, splitters, baseValue.Length);
+
+            var countOfSplitters = WordWrap.GetCountOfNonSpaceSplitters(ref splitters);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(countOfSplitters, Is.EqualTo(3));
+                Assert.That(splitters, Has.Length.EqualTo(3));
+                Assert.That(splitters, Is.EqualTo(baseValue));
+            });
+        }
+
+        [Test]
+        public void GetCountOfNonSpaceSplitters_SomeValuesAreValid_ReturnsCountOfValidSplitters()
+        {
+            var baseValue = new string[] { "tes t", "equals", "nu\nll", "some value", "valid\tvalue" };
+            var splitters = new string[baseValue.Length];
+            Array.Copy(baseValue, splitters, baseValue.Length);
+
+            var countOfSplitters = WordWrap.GetCountOfNonSpaceSplitters(ref splitters);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(countOfSplitters, Is.EqualTo(2));
+                Assert.That(splitters, Has.Length.EqualTo(2));
+                Assert.That(splitters, Is.Not.EqualTo(baseValue));
+            });
+        }
+
+        [Test]
+        public void Wrap_StringToWrapIsNull_ThrowNullReferenseException()
+        {
+            Assert.That(() => WordWrap.Wrap(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Wrap_StringToWrapIsOkSplittersAreNull_ReturnsOriginalString()
+        {
+            var stringToWrap = "Some awesome string";
+
+            var wrappedString = WordWrap.Wrap(stringToWrap, null);
+
+            Assert.That(wrappedString, Is.EqualTo(stringToWrap));
+        }
+
+        [Test]
+        public void Wrap_AllValuesAreOk_ReturnsWrappedString()
+        {
+            var stringToWrap = "Some awesome string";
+            var expectedString = "Some awesome\nstring";
+
+
+            var wrappedString = WordWrap.Wrap(stringToWrap, "awesome");
+
+            Assert.That(wrappedString, Is.EqualTo(expectedString));
+        }
+
+        [Test]
+        public void Wrap_StringToWrapIsOkSplittersAreNotValid_ReturnsWrappedString()
+        {
+            var stringToWrap = "Some awesome string";
+
+
+            var wrappedString = WordWrap.Wrap(stringToWrap, "awesome ", "Some\n");
+
+            Assert.That(wrappedString, Is.EqualTo(stringToWrap));
+        }
+    }
+}
---
+++ UnitTesting/WordWrap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public static class WordWrap
+    {
+        public static string Wrap(string stringToWrap, params string[] splitters)
+        {
+            if (stringToWrap == null)
+            {
+                throw new ArgumentNullException(nameof(stringToWrap));
+            }
+            if (splitters == null)
+            {
+                return stringToWrap;
+            }
+
+            var count = GetCountOfNonSpaceSplitters(ref splitters);
+
+            var wrapedString = stringToWrap;
+            for (int i = 0; i < count; i++)
+            {
+                wrapedString = wrapedString.Replace($"{splitters[i]} ", $"{splitters[i]}\n");
+            }
+
+            return wrapedString;
+        }
+
+        public static int GetCountOfNonSpaceSplitters(ref string[] splitters)
+        {
+            if (splitters == null)
+            {
+                return 0;
+            }
+
+            var newSplitters = new List<string>();
+            for (int i = 0; i < splitters.Length; i++)
+            {
+                if (!splitters[i].Contains(' ') && !splitters[i].Contains('\n'))
+                {
+                    newSplitters.Add(splitters[i]);
+                }
+            }
+
+            splitters = newSplitters.ToArray();
+
+            return splitters.Length;
+        }
+    }
+}
