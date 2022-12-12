461c1b9dd9d0585c338fc0104abec3bc6908412f
Roman Nehaikov <Roman_Nehaikov@epam.com>
2022-12-12 08:54:24 +0300

    Logging

---
+++ Logging/BrainstormSessions/Controllers/SessionController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using BrainstormSessions.Core.Interfaces;
+using BrainstormSessions.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace BrainstormSessions.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly IBrainstormSessionRepository _sessionRepository;
+        private readonly ILogger _logger;
+
+        public SessionController(IBrainstormSessionRepository sessionRepository, ILogger logger)
+        {
+            _sessionRepository = sessionRepository;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (!id.HasValue)
+            {
+                _logger.Error("Id has no value");
+                return RedirectToAction(actionName: nameof(Index),
+                    controllerName: "Home");
+            }
+
+            _logger.Debug("Id has value {Id}", id);
+            var session = await _sessionRepository.GetByIdAsync(id.Value);
+            if (session == null)
+            {
+                _logger.Error("Session is null");
+                return Content("Session not found.");
+            }
+
+            var viewModel = new StormSessionViewModel()
+            {
+                DateCreated = session.DateCreated,
+                Name = session.Name,
+                Id = session.Id
+            };
+
+            _logger.Debug("Ok response");
+            return View(viewModel);
+        }
+    }
+}
